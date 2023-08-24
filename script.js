b9a0afb49de4c0991c2e02469fe138f78c0dc206
sahil <sahil.98@hotmail.com>
2023-08-24 23:13:03 +0100

    Added nav btn listener and included toggle class function for nav-active class

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const navSlide = ()=> {
     const burger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav-links');
 
+burger.addEventListener('click', ()=>{
+    nav.classList.toggle('nav-active');
 
-
-};
-
+});
+}
 
 
 navSlide();

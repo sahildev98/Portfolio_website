94b820b84752fbcf76ce0471d6cae17f20aa1395
sahil <sahil.98@hotmail.com>
2023-08-25 22:55:31 +0100

    Created projectHideBtn const and added classlist toggle function to hide project button when burger is clicked

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const navSlide = ()=> {
     const burger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav-links');
+    const projectHideBtn = document.getElementById('project-btn')
 
 burger.addEventListener('click', ()=>{
     nav.classList.toggle('nav-active');
+    projectHideBtn.classList.toggle('hide-project-btn');
+
 
 });
 }
